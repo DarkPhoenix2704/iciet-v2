25377aad64b03e840be42ade6f8cec651822327c
SidhardhCR <sidhardhcr007@gmail.com>
2022-10-11 22:35:05 +0530

    template added

--- pages/submission.tsx
+++ pages/submission.tsx
@@ -46,7 +46,7 @@ const Submission = () => (
             paddingBlock="18px"
             background="brand.900"
             color="white"
-            onClick={() => fileDownload('/paper-template.docx', 'paper-template.docx')}
+            onClick={() => fileDownload('public/IEEE CONF TEMP A4.docx', 'IEEE CONF TEMP A4.docx')}
             _hover={{ background: 'black', color: 'white' }}
         >
             Paper Template
