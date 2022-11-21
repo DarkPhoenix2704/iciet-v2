9287945163c64c39dfb13e2d554861c2305aec21
Anbarasu <anbarasun123@gmail.com>
2022-11-21 16:23:55 +0530

    Update committe.tsx

--- data/committe.tsx
+++ data/committe.tsx
@@ -1,27 +1,27 @@
 const committe = [
     {
-        role: 'Chief Patron',
+        role: 'Chief Patrons',
         members: [
             {
                 name: 'Er. P H Muneer',
-                role: 'Chairman, Ilahia College of Engineering and Technology',
+                role: 'Chairman, ICET',
             },
             {
                 name: 'Mr. V U Sidhique',
-                role: 'Manager, Ilahia College of Engineering and Technology',
+                role: 'Manager, ICET',
             },
         ],
     },
     {
-        role: 'Patron',
+        role: 'Patrons',
         members: [
             {
                 name: 'Dr. V H Abdul Salam',
-                role: 'Director, Ilahia College of Engineering and Technology',
+                role: 'Director, ICET',
             },
             {
-                name: 'Dr. Faizal M H',
-                role: 'Vice Principal, Ilahia College of Engineering and Technology',
+                name: 'Dr. K A Navas',
+                role: 'Principal, ICET',
             },
         ],
     },
@@ -29,8 +29,8 @@ const committe = [
         role: 'Conference General Chair',
         members: [
             {
-                name: 'Prof. Dr. K A Navas',
-                role: 'Principal, Ilahia College of Engineering and Technology',
+                name: 'Dr. Abdul Gafur M',
+                role: 'Professor(CSE)',
             },
         ],
     },
@@ -39,196 +39,186 @@ const committe = [
         members: [
             {
                 name: 'Prof. Rosna P Haroon',
-                role: 'Asst. Professor(CSE), Coordinator',
+                role: 'Asst. Professor(CSE)',
             },
             {
                 name: 'Prof. Ansar Jamal',
-                role: 'Asst. Professor(EEE), Co-Coordinator',
+                role: 'Asst. Professor(EEE)',
             },
             {
                 name: 'Dr. Sachin Gee Paul',
-                role: 'Assoc. Professor(EEE), Co-Coordinator',
+                role: 'Asso. Professor(EEE),
             },
         ],
     },
     {
-        role: 'Advisory Committe',
+        role: 'Advisory Committee',
         members: [
             {
-                name: 'Dr. P H Muneer',
-                role: 'Chairman, ICET',
-            },
-            {
-                name: 'Mr. KM Pareeth',
-                role: 'General Secretary, Ilahia Trust',
+                name: 'Mr. K M Pareeth',
+                role: 'Chairman, Ilahia Trust',
             },
             {
                 name: 'Mr. M M Muhammed Kunju',
                 role: 'Treasurer, Ilahia Trust',
             },
             {
-                name: 'Mr. V U Sidhique',
-                role: 'Manager, ICET',
-            },
-            {
-                name: 'Prof. Dr. K A Navas',
-                role: 'Principal, ICET',
-            },
-            {
                 name: 'Mr. A A Rahim',
                 role: 'Treasurer, ICET',
             },
             {
-                name: 'The Regional Officer',
-                role: 'SWRO, AICTE, Banglore',
-            },
-            {
-                name: 'The Registrar',
-                role: 'APJ Abdul Kalam Technical University',
-            },
-            {
-                name: 'The Director of Technical University',
-                role: 'Govt. of Kerala',
+                name: "Dr. Faizal M H".
+                role: "Vice Principal, ICET"
             },
             {
                 name: 'Prof. Dr. K P Sudheer',
                 role: 'Professor, IIT Madras',
             },
             {
-                name: 'Dr. M P Sebastin',
+                name: 'Dr. M P Sebastian',
                 role: 'Professor, IIM Kozhikode',
             },
             {
-                name: 'Dr. Abdul Nazeer K A',
-                role: 'Professor, NI Calicut',
-            },
-            {
-                name: 'Dr. Unnikrishanan Cheramangalath',
-                role: 'Asst. Prof. IIT Palakkad',
-            },
-            {
                 name: 'Dr. C H Abdul Buhari',
-                role: 'Former Joint Director, Technical Education Kerala',
+                role: 'Former Joint Director, Technical Education, Kerala',
             },
             {
                 name: 'Mr. M A Mohammed Ali',
                 role: 'Former Director, Cochin Oil Refinery',
             },
             {
-                name: 'Dr. Abdul Salam V H',
-                role: 'Director, ICET',
-            },
-            {
                 name: 'Mr. N Jehangir',
-                role: 'Vice Chairman & MD, Eastern Group of Companies',
+                role: 'Vice Chairman & MD, Nest Group of Companies',
             },
             {
                 name: 'Mr. Navaz Meeran',
-                role: 'Vice Chairman, Eastern Group of Companies',
+                role: 'Chairman, Eastern Group of Companies',
             },
-
             {
-                name: 'Rosna P Haroon',
-                role: 'Asst. Prof. ICET',
+                name: 'Dr. Jaison Mathew',
+                role: 'Asso. Prof. GEC Thrissur',
             },
+            {
+                name: "Dr. Mini Ulanat",
+                role: "Section Chair, IEEE Kerala Section"
+            },
+            {
+                name: "Dr. Chinmoy Saha",
+                role: "Chairman, IEEE Microwave Theory and Technics Chapter, IEEE Kerala Section"
+            },
+            {
+                name: "Dr. Biju K",
+                role: "Treasurer, IEEE Kerala Section"
+            }
         ],
     },
     {
-        role: 'Technical Committe - External',
+        role: 'Technical Committee',
         members: [
             {
-                name: 'Dr. Raman Unnikrishnan',
-                role: 'Professor, California State University, Fullerton',
+                name: 'Dr. Raman Menon Unnikrishnan',
+                role: 'Professor, California State University, Fullerton, California',
             },
             {
-                name: 'Dr. Tareq Ahimedat',
-                role: 'Head of Robotics & AI, Univaersity of Tabok, KSA',
+                name: 'Dr. Tareq Alhmiedat',
+                role: 'Head of Robotics & AI, University of Tabuk, Kingdom of Saudi Arabia',
             },
             {
                 name: 'Dr. Vinod Pangracious',
-                role: 'Assoc. Prof. American University, Dubai',
+                role: 'Asso. Prof. American University, Dubai',
             },
             {
-                name: 'Dr. Azeemsha Thachampoyil',
-                role: 'Senior Embedded Software Engineer, UK',
-            },
-            {
-                name: 'Dr. K P Sudheer',
-                role: 'Professor, IIT Madras',
-            },
-            {
-                name: 'Dr. Isha TB',
-                role: 'Professor, Amritha Vishvavidya Peetham, Coimbatore',
+                name: 'Dr. Azeemsha Thacham Poyil',
+                role: 'Senior Embedded Software Engineer, Vectura Limited, United Kingdom',
             },
             {
                 name: 'Dr. John Jose',
-                role: 'Assistant Professor, Sreekrishna College of Engineering, Coimbatore',
+                role: 'Asso. Prof. IIT Guwahati',
             },
             {
-                name: 'Dr. Barakkath Nisha',
-                role: 'Associate Professor, Sreekrishna College of Engineering, Coimbatore',
-            },
-            {
-                name: 'Dr. Unnikrishnan',
-                role: 'Cheramangalath Assistant Professor, IIT Palakkad',
-            },
-            {
-                name: 'Dr. M P Sebastin',
-                role: 'Professor, IIM Kozhikode',
+                name: 'Dr. Unnikrishnan Cheramangalath',
+                role: 'Asst. Prof., IIT Palakkad',
             },
             {
                 name: 'Dr. S M Sameer',
                 role: 'Professor, NIT Calicut',
             },
             {
-                name: 'George K Varghese',
+                name: 'Dr. George K Varghese',
                 role: 'Professor, NIT Calicut',
             },
             {
                 name: 'Dr. Shobana N V',
                 role: 'Professor, RIT Kottayam',
             },
-        ],
-    },
-    {
-        role: 'Technical Committe - Internal',
-        members: [
             {
-                name: 'Prof. Dr. K A Navas',
-                role: 'Principal, ICET',
+                name: "Dr. Anilkumar C V",
+                role: "Professor, College of Engineering, Karunagappaly"
             },
             {
-                name: 'Dr. Faizal M H',
-                role: 'Vice Principal, ICET',
+                name: "Er. Jithin Krishnan",
+                role: "Scientist, SCTIMST, Thiruvananthapuram"
+            },
+            {
+                name: "Dr. Isha T B",
+                role: "Prof. Amrita Viswa Vidyapeetham, Coimbatore"
+            },
+            {
+                name: "Dr. Jubilant Job",
+                role: "Prof., Saintgits College of Engineering, Kottayam"
+            },
+            {
+                name: "Dr. Barakkath Nisha U",
+                role: "Asso. Prof., Sreekrishna College of Engineering, Coimbatore"
+            },
+            {
+                name: "Dr. Varghese Paul",
+                role: "Prof., Rajagiri School of Engineering & Technology, Ernakulam"
+            },
+            {
+                name: "Dr. Janahanlal Stephen",
+                role: "Retd. Prof., M A College of Engineering, Kothamangalam"
             },
             {
                 name: 'Dr. Laju Kottallil',
-                role: 'Professor, Dept. of Civil Engineering',
+                role: 'Professor, ICET',
             },
             {
                 name: 'Dr. Benny Paul',
-                role: 'Professor, Dept. of Mechanical Engineering',
+                role: 'Professor, ICET',
             },
             {
                 name: 'Dr. Sujith Kumar P S',
-                role: 'Professor, Dept. of CSE',
+                role: 'Professor, ICET',
             },
             {
                 name: 'Dr. Vadhanakumari S',
-                role: 'Professor, Dept of CSE',
+                role: 'Professor, ICET',
             },
             {
-                name: 'Syed Ibrahim',
-                role: 'Asso. Prof, Civil Engineering',
+                name: 'Dr. S Syed Ibrahim',
+                role: 'Asso. Prof, ICET',
             },
             {
                 name: 'Dr. Abhiraj T K',
-                role: 'Asso. Prof, Dept of EEE',
+                role: 'Asso. Prof, ICET',
             },
             {
                 name: 'Dr. A Jahubar Ali',
-                role: 'Professor, Dept of Science & Humanities',
+                role: 'Professor, ICET',
             },
+            {
+                name: "Dr. Jinu Mohan", 
+                role: "Asso. Prof., ICET"
+            },
+            {
+                name: "Dr. P A Habeeb Rahiman",
+                role: "Professor, ICET"
+            },
+            {
+                name: "Dr. Shebin Sharief",
+                role: "Asso. Prof., ICET"
+            }
         ],
     },
 ];
