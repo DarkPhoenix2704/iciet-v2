a474f05b4d6be6058c43599bd6dfc3279ecbefe7
Anbarasu <anbarasun123@gmail.com>
2022-11-27 10:15:50 +0530

    add header

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,9 +7,15 @@ import Layout from '../layout';
 import Header from '../public/Header.png';
 import theme from '../theme';
 import '@fontsource/open-sans';
+import Head from 'next/head';
 
 const MyApp = ({ Component, pageProps }: { Component: any; pageProps: any }) => (
     <ChakraProvider theme={theme}>
+        <Head>
+            <title>
+                IEEE International Conference on Innovation In Engineering and Technology 2023
+            </title>
+        </Head>
         <Image src={Header} alt="Header" width="100vw" />
         <Dates />
         <NavBar />
