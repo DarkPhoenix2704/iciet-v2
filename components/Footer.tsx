736cf49b07b9d4c72fb8d51bf26b6689c4c54edc
DarkPhoenix2704 <anbarasun123@gmail.com>
2023-01-16 19:40:55 +0530

    foooooterrrrrrrrrrrrrrr

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,10 +1,22 @@
-import { Box, Text, Flex } from '@chakra-ui/react';
+import { Box, Text, Flex, Icon } from '@chakra-ui/react';
+import { FaFacebook, FaInstagram } from 'react-icons/fa';
 import React from 'react';
+import Link from 'next/link';
 
 const Footer = () => (
     <Box bg="#333333">
         <Box paddingBlock="5px">
-            <Flex justifyContent="center">
+            <Flex
+                flexDirection="row"
+                paddingBlock={{
+                    base: '10px',
+                }}
+                justifyContent="space-between"
+                paddingInline={{
+                    base: '10px',
+                    md: '50px',
+                }}
+            >
                 <Text
                     fontSize={{
                         base: '16px',
@@ -14,6 +26,26 @@ const Footer = () => (
                 >
                     ICIET 23
                 </Text>
+                <Flex flexDirection="row" columnGap="15px">
+                    <Link href="https://www.facebook.com/Iceterkm">
+                        <Icon
+                            cursor="pointer"
+                            height="25px"
+                            width="25px"
+                            as={FaFacebook}
+                            color="white"
+                        />
+                    </Link>
+                    <Link href="https://www.instagram.com/ilahiaicet/">
+                        <Icon
+                            cursor="pointer"
+                            height="25px"
+                            width="25px"
+                            as={FaInstagram}
+                            color="white"
+                        />
+                    </Link>
+                </Flex>
             </Flex>
         </Box>
     </Box>
